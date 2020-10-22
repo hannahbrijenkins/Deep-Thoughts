d6afa5550ce4eeef62525fad49d6cc112c3e2460
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-10-21 21:39:11 -0400

    added full stack structure, middleware, scripts for server launch

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
 
 // middleware
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth')
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -23,6 +24,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
